d6d9edd2e3f62526f80451a65386bd0cb2b139d3
Reinhard Stahn <reinhardstahn@t-online.de>
2023-02-10 08:25:20 +0100

    Refactor build-site.el

--- build-site.el
+++ build-site.el
@@ -3,30 +3,50 @@
 ;; Set the package installation directory so that packages aren't stored in the
 ;; ~/.emacs.d/elpa path.
 (require 'package)
-(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-user-dir (expand-file-name "./.packages")
+      package-archive-priorities '(("gnu" . 30) ("nongnu" . 20) ("melpa" . 10)))
 
-;; Load packages but do not activate them.
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; Load packages but do not activate them:
 (package-initialize nil)
 
 ;; Activate dependencies, ensure installation if necessary
 (setq use-package-verbose t)
+(use-package esxml :ensure) ; use s-expressions to write xml (or html)
 (use-package htmlize :ensure)
 (use-package org)
 (use-package ox-publish)
 
+
 (message "Using org-mode version: %s" org-version)
 
-(setq org-html-html5-fancy t
-    org-html-validation-link t  ;; TODO
-    org-html-head-include-scripts nil
-    org-html-head-include-default-style nil
-    org-html-head (concat
-                      "<link rel=\"stylesheet\" href=\"./css/simple.css\" />\n"
-                      "<link rel=\"stylesheet\" href=\"./css/style.css\" />\n")
-    )
 
-(setq python-indent-guess-indent-offset-verbose nil ; otherwise we get tons of "Can’t guess python-indent-offset, using defaults: 4"
-    org-cite-global-bibliography (list (expand-file-name "./bibliography.bib")))
+(setq org-html-html5-fancy t
+      ;;org-html-validation-link nil  ;; TODO
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-head (concat
+                     "<link rel=\"stylesheet\" href=\"./css/simple.css\" />\n"
+                     "<link rel=\"stylesheet\" href=\"./css/style.css\" />\n")
+      org-html-postamble #'rs/site-footer
+      org-cite-global-bibliography (list (expand-file-name "./bibliography.bib"))
+      ;; Otherwise we get tons of "Can’t guess python-indent-offset, using defaults: 4":
+      python-indent-guess-indent-offset-verbose nil)
+
+
+(defun rs/site-footer (options)
+  (let ((creator (plist-get options :creator))
+        (foo (plist-get options :foo)))
+    (esxml-to-xml ; from esxml
+     `(footer ((class . "footer"))
+              (p nil
+                 "Made by " (a ((href . "https://github.com/rainij")) "Reinhard Stahn")
+                 " with " (raw-string ,creator))
+              (p nil
+                 "Sources on " ;; TODO: set link once repo is on github
+                 (a ((href . "https://github.com/rainij"))
+                    "Github"))))))
 
 (setq rs/default-publish-params
       (list
@@ -34,17 +54,15 @@
        :base-directory "./src"
        :publishing-directory "./public"))
 
+
 (setq org-publish-project-alist
     `(("pages"
        ,@rs/default-publish-params
        :base-extension "org"
        :publishing-function org-html-publish-to-html
-       :with-author nil
-       :with-creator t ;; shows emacs and org version
        :headline-levels 6
        :section-numbers nil
        :time-stamp-file t
-       ;;:completion-function '(copy-images copy-css)
        :with-inlinetasks nil
        :with-toc 3)
       ("static"
@@ -52,6 +70,7 @@
        :base-extension "css\\|svg"
        :publishing-function org-publish-attachment)
       ("all" :components ("pages" "static"))))
+
 
 (org-publish "all" t)
 (message "DONE")
