9a4d3d4b6bcacbf7be2280d39abb0c2e643cd7e5
Reinhard Stahn <reinhardstahn@t-online.de>
2023-02-10 15:20:44 +0100

    Improve build-site.el. In particular using custom html template now

--- build-site.el
+++ build-site.el
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S emacs --script
+#!/usr/bin/env -S emacs --funcall rs/publish --script
 
 ;; Set the package installation directory so that packages aren't stored in the
 ;; ~/.emacs.d/elpa path.
@@ -11,6 +11,8 @@
 ;; Load packages but do not activate them:
 (package-initialize nil)
 
+(load-theme 'misterioso t) ; TODO: does not effect syntax highlighting in batch mode
+
 ;; Activate dependencies, ensure installation if necessary
 (setq use-package-verbose t)
 (use-package esxml :ensure) ; use s-expressions to write xml (or html)
@@ -22,31 +24,49 @@
 (message "Using org-mode version: %s" org-version)
 
 
-(setq org-html-html5-fancy t
-      ;;org-html-validation-link nil  ;; TODO
-      org-html-head-include-scripts nil
-      org-html-head-include-default-style nil
-      org-html-head (concat
-                     "<link rel=\"stylesheet\" href=\"./css/simple.css\" />\n"
-                     "<link rel=\"stylesheet\" href=\"./css/style.css\" />\n")
-      org-html-postamble #'rs/site-footer
-      org-cite-global-bibliography (list (expand-file-name "./bibliography.bib"))
+(setq org-cite-global-bibliography (list (expand-file-name "./bibliography.bib"))
+      org-export-use-babel nil ; don't execute source blocks while exporting
+      org-html-html5-fancy t
       ;; Otherwise we get tons of "Can’t guess python-indent-offset, using defaults: 4":
       python-indent-guess-indent-offset-verbose nil)
 
 
-(defun rs/site-footer (options)
-  (let ((creator (plist-get options :creator))
-        (foo (plist-get options :foo)))
-    (esxml-to-xml ; from esxml
-     `(footer ((class . "footer"))
-              (p nil
-                 "Made by " (a ((href . "https://github.com/rainij")) "Reinhard Stahn")
-                 " with " (raw-string ,creator))
-              (p nil
-                 "Sources on " ;; TODO: set link once repo is on github
-                 (a ((href . "https://github.com/rainij"))
-                    "Github"))))))
+(defun rs/site-footer (info)
+  (let ((creator (plist-get info :creator)))
+    `(footer ((class . "footer"))
+             (p nil
+                "Made by " (a ((href . "https://github.com/rainij")) "Reinhard Stahn")
+                " with " (raw-string ,creator))
+             (p nil
+                "Find the source code on " ;; TODO: set link, once repo is on github
+                (a ((href . "https://github.com/rainij")) "Github")))))
+
+;; This is our custom html template (written as s-expression):
+(defun rs/org-html-template (contents info)
+  (let ((title (org-export-data (and (plist-get info :with-title) (plist-get info :title)) info))
+        (mathjax (plist-get info :rs/mathjax)))
+    (concat
+     "<!DOCTYPE html>"
+     (esxml-to-xml
+      `(html ((lang . "en"))
+             (head nil
+                   (meta ((charset . "utf-8")))
+                   (meta ((name . "viewport") (content . "width=device-width, initial-scale=1.0")))
+                   (meta ((name . "author") (content . "Reinhard Stahn")))
+                   (title nil ,title)
+                   (link ((rel . "stylesheet") (href . "./css/simple.css")))
+                   (link ((rel . "stylesheet") (href . "./css/style.css")))
+                   (script ((src . "./js/perf.js")) "")
+                   (script ((src . "./js/mathjax.js")) "")
+                   (script ((async . "")
+                            (src . ,(plist-get mathjax :url))
+                            (integrity . ,(plist-get mathjax :integrity))
+                            (crossorigin . "anonymous"))
+                           ""))
+             (body nil
+                   (h1 ((class . "title")) ,title)
+                   (raw-string ,contents)
+                   ,(rs/site-footer info)))))))
 
 (setq rs/default-publish-params
       (list
@@ -55,22 +75,37 @@
        :publishing-directory "./public"))
 
 
+(org-export-define-derived-backend 'rs/pages-html 'html
+  :translate-alist
+  '((template . rs/org-html-template)))
+
+;; Override this function with our own backend
+(defun org-html-publish-to-html (plist filename pub-dir)
+  (org-publish-org-to 'rs/pages-html filename ".html" plist pub-dir))
+
+
 (setq org-publish-project-alist
     `(("pages"
        ,@rs/default-publish-params
+       :rs/mathjax ,(list
+                     :url "https://cdn.jsdelivr.net/npm/mathjax@3.2.2/es5/tex-mml-chtml.js"
+                     :integrity "sha384-Wuix6BuhrWbjDBs24bXrjf4ZQ5aFeFWBuKkFekO2t8xFU0iNaLQfp2K6/1Nxveei")
        :base-extension "org"
        :publishing-function org-html-publish-to-html
        :headline-levels 6
        :section-numbers nil
        :time-stamp-file t
        :with-inlinetasks nil
-       :with-toc 3)
+       :with-toc 2)
       ("static"
        ,@rs/default-publish-params
-       :base-extension "css\\|svg"
+       :base-extension "css\\|js\\|svg"
        :publishing-function org-publish-attachment)
       ("all" :components ("pages" "static"))))
 
 
-(org-publish "all" t)
-(message "DONE")
+;; Wrapping the publishing action in a function to be able to load the file into emacs.
+(defun rs/publish ()
+  (interactive)
+  (org-publish-all t)
+  (message "DONE"))
