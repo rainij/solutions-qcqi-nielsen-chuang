69c044852bf11593044bde90047bedf0856ada70
Reinhard Stahn <reinhardstahn@t-online.de>
2023-02-26 21:12:02 +0100

    Added a HOME link to the website

--- build-site.el
+++ build-site.el
@@ -47,6 +47,9 @@
                 "Find the source code on " ;; TODO: set link, once repo is on github
                 (a ((href . "https://github.com/rainij/solutions-qcqi-nielsen-chuang")) "Github")))))
 
+(defun rs/nav (info)
+  `(nav nil (a ((href . "/")) "HOME")))
+
 ;; This is our custom html template (written as s-expression):
 (defun rs/org-html-template (contents info)
   (let ((title (org-export-data (and (plist-get info :with-title) (plist-get info :title)) info))
@@ -71,6 +74,7 @@
                             (crossorigin . "anonymous"))
                            ""))
              (body nil
+                   ,(rs/nav info)
                    (h1 ((class . "title")) ,title)
                    (raw-string ,contents)
                    ,(rs/site-footer info)))))))
