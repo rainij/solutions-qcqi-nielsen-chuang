d7f1b3b0db646bb1e6ed052edc3cacb13135b0b8
Reinhard Stahn <reinhardstahn@t-online.de>
2023-03-19 19:52:39 +0100

    minimal cleanup

--- bin/common.sh
+++ bin/common.sh
@@ -4,9 +4,9 @@ set -eu
 
 # .use_feynman is *not* under vc. Create this file with a single line
 # "FEYNMAN_USER_ID=<your UID>" to use the scripts run-*-in-docker.sh locally without
-# permission conflicts (so that generated files/folders like ./public/ have the correct
-# owner). This may be convenient for example if you do not have emacs installed but still
-# want to tangle all files or build the site locally.
+# permission conflicts (so that generated files/directories like ./public/ have the
+# correct owner). This may be convenient for example if you do not have emacs installed
+# but still want to tangle all files or build the site locally.
 if [ -f $(dirname $0)/.use_feynman ]; then
     source $(dirname $0)/.use_feynman
     ADDITIONAL_BUILD_ARGS="--build-arg FEYNMAN_USER_ID=$FEYNMAN_USER_ID"
