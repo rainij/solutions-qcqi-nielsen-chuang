0aa668f03c0e1594116800adb2c9582211a157b4
Reinhard Stahn <reinhardstahn@t-online.de>
2023-03-19 16:43:45 +0100

    Rename .env-local into .use_feynman

--- bin/common.sh
+++ bin/common.sh
@@ -2,9 +2,13 @@
 
 set -eu
 
-# .env-local  is *not* under vc
-if [ -f $(dirname $0)/.env-local ]; then
-    source $(dirname $0)/.env-local
+# .use_feynman is *not* under vc. Create this file with a single line
+# "FEYNMAN_USER_ID=<your UID>" to use the scripts run-*-in-docker.sh locally without
+# permission conflicts (so that generated files/folders like ./public/ have the correct
+# owner). This may be convenient for example if you do not have emacs installed but still
+# want to tangle all files or build the site locally.
+if [ -f $(dirname $0)/.use_feynman ]; then
+    source $(dirname $0)/.use_feynman
     ADDITIONAL_BUILD_ARGS="--build-arg FEYNMAN_USER_ID=$FEYNMAN_USER_ID"
     ADDITIONAL_RUN_ARGS="--user feynman"
 else
