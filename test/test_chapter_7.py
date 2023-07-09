8ca20fc9b455ce72754abb9adc767e700a6e8670
Reinhard Stahn <reinhardstahn@t-online.de>
2023-07-09 21:51:35 +0200

    Slightly improve chapter 7 and its tests

--- test/test_chapter_7.py
+++ test/test_chapter_7.py
@@ -1,6 +1,8 @@
 from sage.all import i
 
-from chapter_7_sage import x, p, a, ad
+from chapter_7_sage import x, dx, p, a, ad
 
-def test_position_and_momentum_operator():
+def test_position_momentum_ladder_operator():
+    assert p == -i*dx
     assert x*p - p*x == i
+    assert a*ad - ad*a == 1
