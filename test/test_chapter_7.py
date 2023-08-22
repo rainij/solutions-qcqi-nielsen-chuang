de214a0f19ee11f37d1d3344f60a92bd7360ca51
Reinhard Stahn <reinhardstahn@t-online.de>
2023-08-22 17:36:27 +0200

    Added exercisees 7.15 and 7.16

--- test/test_chapter_7.py
+++ test/test_chapter_7.py
@@ -1,6 +1,6 @@
 from sage.all import i, pi, matrix, sqrt
 
-from chapter_7_sage import x, dx, p, a, ad, B
+from chapter_7_sage import x, dx, p, a, ad, B, Pl, Dn, lambda_nl
 
 
 def test_position_momentum_ladder_operator():
@@ -13,3 +13,10 @@ def test_beamsplitter():
     assert B.H * B == matrix.identity(2), "Should be orthogonal"
     assert B.subs(theta=pi/4) == matrix([[1, -1], [1, 1]]) / sqrt(2), \
         "Should be parameterized by theta in the right way"
+
+
+def test_exercise_716():
+    for l in range(6):
+        for n in range(l+1):
+            q = Dn(Pl(l), n) - lambda_nl(n, l)*Pl(l)
+            assert q == 0
