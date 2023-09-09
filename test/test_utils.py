7c31492342ec4d62830966d0c3c98354413237f7
Reinhard Stahn <reinhardstahn@t-online.de>
2023-09-09 21:49:11 +0200

    Added exercises 7.41, 7.42, and 7.47 - 7.51

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,11 +1,32 @@
-from sage.all import matrix, i
+from sage.all import matrix, i, sqrt
 
-from utils_sage import Id, X, Y, Z, H
+from utils_sage import Id, X, Y, Z, H, S, P0, P1, CX, CY, CZ, kron
 
 
 def test_pauli_matrices():
     # Basic algebraic identities:
     assert Id == matrix.identity(2)
-    assert X**2 == Y**2 == Z**2 == -i*X*Y*Z == Id
+    assert Z == matrix.diagonal([1, -1])
+
     assert H.H == H
-    assert H*X*H == Z
+    assert H*H == Id
+    assert H == (X + Z) / sqrt(2)
+    assert X == H*Z*H
+
+    assert X**2 == Y**2 == Z**2 == -i*X*Y*Z == Id
+
+
+def test_phase_gate():
+    assert S**2 == Z
+    assert S * X * S.H == Y
+
+
+def test_projections():
+    assert P0 == matrix([[1, 0], [0, 0]])
+    assert P1 == matrix([[0, 0], [0, 1]])
+
+
+def test_controlled_paulis():
+    assert CZ == matrix.diagonal([1, 1, 1, -1])
+    assert CX == kron(Id, H) * CZ * kron(Id, H)
+    assert CY == kron(Id, S) * CX * kron(Id, S.H)
